cb8ad9aa60536ef005a0ad21b7c1958a17664c7b
Malay Acharya <18acharyamalay@gmail.com>
2022-01-26 00:54:38 +0530

    back

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,12 +5,22 @@ $(".enter").on("click", function () {
   t2.play();
 });
 
+$(".back").on("click", function () {
+  console.log("hello");
+  t2.reverse();
+});
+
 //timeline for select your gender bg
 
 t2 = new TimelineMax({ paused: true });
 
 t2.to(".genderSection", 1, { top: "0%", ease: Power4.easeOut })
-  .from(".genderText", 1, { y: "100%", opacity: 0, ease: Power2.easeOut }, 0.5)
-  .from(".selRad", 1, { x: "-100%", opacity: 0, ease: Power3.easeOut}, 1.25)
-  .from(".genderLabel", 1, { x: "7%", opacity: 0, ease: Power3.easeOut}, 1.2)
-  .from('.back', .7, {opacity:0})
+.from(".genderText", 1, { y: "100%", opacity: 0, ease: Power2.easeOut }, 0.5)
+.from(".selRad", 1, { x: "-100%", opacity: 0, ease: Power3.easeOut}, 1.25)
+.from(".genderLabel", 1, { x: "7%", opacity: 0, ease: Power3.easeOut}, 1.2)
+.from('.back', .7, {opacity:0})
+
+//directory text
+
+gsap.registerPlugin(SplitText);
+var mySplitText = new SplitText(".dirText", {type: 'chars'})
