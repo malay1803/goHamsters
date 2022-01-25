4780a53bcdc3039232231c61fe414514c26c9469
Malay Acharya <18acharyamalay@gmail.com>
2022-01-25 22:54:58 +0530

    section

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,10 +1,19 @@
 // --------nextsection--------
 
-$(".enter").on('click', function(){
-  console.log("hello")
-  genderSection()
-})
+$(".enter").on("click", function () {
+  console.log("hello");
+  t1.play();
+});
 
-function genderSection(){
-  gsap.to(".genderSection", 1, {top: "0%"})
-}
+//timeline for select your gender bg
+
+t1 = new TimelineMax({ paused: true });
+
+t1
+.to(".genderSection", 1, { top: "0%", ease: Power4.easeOut })
+.from(
+  ".genderText",
+  1,
+  { y: "100%", opacity: 0, ease: Power2.easeOut },
+  0.5
+);
