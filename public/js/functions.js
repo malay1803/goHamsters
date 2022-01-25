27616d38eda4aae9e2e500a1d0f8268e7fddaf75
Malay Acharya <18acharyamalay@gmail.com>
2022-01-26 00:32:21 +0530

    select gender

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,18 +2,15 @@
 
 $(".enter").on("click", function () {
   console.log("hello");
-  t1.play();
+  t2.play();
 });
 
 //timeline for select your gender bg
 
-t1 = new TimelineMax({ paused: true });
+t2 = new TimelineMax({ paused: true });
 
-t1
-.to(".genderSection", 1, { top: "0%", ease: Power4.easeOut })
-.from(
-  ".genderText",
-  1,
-  { y: "100%", opacity: 0, ease: Power2.easeOut },
-  0.5
-);
+t2.to(".genderSection", 1, { top: "0%", ease: Power4.easeOut })
+  .from(".genderText", 1, { y: "100%", opacity: 0, ease: Power2.easeOut }, 0.5)
+  .from(".selRad", 1, { x: "-100%", opacity: 0, ease: Power3.easeOut}, 1.25)
+  .from(".genderLabel", 1, { x: "7%", opacity: 0, ease: Power3.easeOut}, 1.2)
+  .from('.back', .7, {opacity:0})
