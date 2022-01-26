2301bee3ac0706bf1b418d1d368cbdc3fad5d6a7
Malay Acharya <18acharyamalay@gmail.com>
2022-01-26 12:57:58 +0530

    m

--- public/js/functions.js
+++ public/js/functions.js
@@ -15,12 +15,18 @@ $(".back").on("click", function () {
 t2 = new TimelineMax({ paused: true });
 
 t2.to(".genderSection", 1, { top: "0%", ease: Power4.easeOut })
-.from(".genderText", 1, { y: "100%", opacity: 0, ease: Power2.easeOut }, 0.5)
-.from(".selRad", 1, { x: "-100%", opacity: 0, ease: Power3.easeOut}, 1.25)
-.from(".genderLabel", 1, { x: "7%", opacity: 0, ease: Power3.easeOut}, 1.2)
-.from('.back', .7, {opacity:0})
+  .from(".genderText", 1, { y: "100%", opacity: 0, ease: Power2.easeOut }, 0.5)
+  .from(".selRad", 1, { x: "-100%", opacity: 0, ease: Power3.easeOut }, 1.25)
+  .from(".genderLabel", 1, { x: "7%", opacity: 0, ease: Power3.easeOut }, 1.2)
+  .from(".back", 0.7, { opacity: 0 });
 
 //directory text
 
-gsap.registerPlugin(SplitText);
-var mySplitText = new SplitText(".dirText", {type: 'chars'})
+t3 = new TimelineMax({});
+
+t3.from(".dirText", {
+  duration: 1.5,
+  y: "100%",
+  ease: Power4.easeOut,
+  stagger: 0.1,
+});
