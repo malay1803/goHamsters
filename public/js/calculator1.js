22b169b3ebeab85a0845963b4ee5226db74a3d10
Pranav Jain <pranavjainmrt@gmail.com>
2022-02-15 20:12:38 +0530

    cal

--- public/js/calculator1.js
+++ public/js/calculator1.js
@@ -9,7 +9,6 @@ $("#slider1").roundSlider({
     rangeColor:"#30475E",
     pathColor:"#f05454",
     change: function (e) {
-        console.log("hello1", e.value)
         $('.ageValue').html(e.value)
     }
 });
@@ -23,7 +22,6 @@ $("#slider2").roundSlider({
     rangeColor:"#30475E",
     pathColor:"#f05454",
     change: function (e) {
-        console.log("hello1", e.value)
         $('.heightValue').html(e.value)
     }
 });
@@ -37,31 +35,49 @@ $("#slider3").roundSlider({
     rangeColor:"#30475E",
     pathColor:"#f05454",
     change: function (e) {
-        console.log("hello1", e.value)
         $('.weightValue').html(e.value)
     }
 });
 
 // CALCULATION
-console.log($('input[name="gender"]:checked').val())
 
-$(".gender").on("click", ()=>{
-    
+function CalCalculator(){
     const gender=$('input[name="gender"]:checked').val()
     const age= $('.ageValue').text()
     const height= $('.heightValue').text()
     const weight= $('.weightValue').text()
+    const sel=$('#frequency').val()
     let BMR
 
     console.log("hello2",age)
     if(gender==="male"){
-        BMR = Math.ceil( (10 * weight) + (6.25 * height) - (5 * age) + 5 )
-        console.log(BMR)
+        BMR = ( (10 * weight) + (6.25 * height) - (5 * age) + 5 )
     }
     else if(gender==="female"){
-        BMR = Math.ceil( (10 * weight) + (6.25 * height) - (5 * age) - 161 )
-        console.log(BMR)
+        BMR = Math.ceil( (10 * weight) + (6.25 * height) - (5 * age) - 161 ) 
     }
-
-})
+    if(sel==1){
+        BMR = BMR*1; 
+    }
+    else if(sel==1.2){
+        BMR = BMR*1.2;
+    }
+    else if(sel==1.375){
+        BMR = BMR*1.375;
+    }
+    else if(sel==1.465){
+        BMR = BMR*1.465;
+    }
+    else if(sel==1.55){
+        BMR = BMR*1.55;
+    }
+    else if(sel==1.725){
+        BMR = BMR*1.725;
+    }
+    else if(sel==1.9){
+        BMR = BMR*1.9;
+    }
+    
+    $('#resultBMR').html(Math.ceil(BMR))
+}
 
