fa887127bfd992e6dc4c4200befa74d61c4ffe03
malay1803 <18acharyamalay@gmail.com>
2022-03-04 11:58:51 +0530

    Malay

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 var bcrypt = require("bcryptjs");
 var salt = bcrypt.genSaltSync(10);
+const request = require("request");
 
 const User = require("./models/users");
-const Excercise = require("./models/excercise")
+const Excercise = require("./models/excercise");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -32,58 +33,52 @@ mongoose
     console.log(err);
   });
 
-const directoryRoute = require("./routes/Directory")
+const directoryRoute = require("./routes/Directory");
 
-app.use("/directory", directoryRoute)
+app.use("/directory", directoryRoute);
 
-
-app.get("/", (req, res) => {
-  res.render("card");
-  // res.render("index");
+app.post("/search", (req, res) => {
+  var query = req.body.searchQuery;
+  request.get(
+    {
+      url: "https://api.calorieninjas.com/v1/nutrition?query=" + query,
+      headers: {
+        "X-Api-Key": "o6l5WsZgJr9hRQRTRauoog==vQ8OewoiWvHVb690",
+      },
+    },
+    function (error, response, body) {
+      if (error) return console.error("Request failed:", error);
+      else if (response.statusCode != 200)
+        return console.error(
+          "Error:",
+          response.statusCode,
+          body.toString("utf8")
+        );
+      else console.log(body);
+    }
+  );
 });
-
-
-// app.get("/directory", async (req, res) => {
-//   //   res.send("hello");
-//   let excerType
-//   let bodyPart = req.query.bodyPart 
-//   console.log( req.query.bodyPart);
-
-//   if(bodyPart==="traps"){
-//     excerType = req.query.type
-//     var excercises = await Excercise.find({excerciseCategory: "shoulder"+req.query.type})
-//   }
-
-//   if(req.query.type === undefined){
-//     excerType = "Stretches"
-//     bodyPart ="traps"
-//     var excercises = await Excercise.find({excerciseCategory: "shoulderStretches"})
-//   }
-//   // const bodyPart = req.params;
-//   res.render("directory", {userName: req.session.name, excercises: excercises, excerType: excerType, bodyPart:bodyPart});
-// });
-
 
 app.get("/directory1", (req, res) => {
   //   res.send("hello");
-  res.render("directory1", {userName: req.session.name});
+  res.render("directory1", { userName: req.session.name });
 });
 
 app.get("/login", (req, res) => {
   //   res.send("hello");
-  res.render("login", {userName: req.session.name});
+  res.render("login", { userName: req.session.name });
 });
 
 app.get("/userDashboard", (req, res) => {
-  res.render("userDashboard", {userName: req.session.name})
+  res.render("userDashboard", { userName: req.session.name });
 });
 
 app.get("/calculator1", (req, res) => {
   //   res.send("hello");
-  res.render("calculator1", {userName: req.session.name});
+  res.render("calculator1", { userName: req.session.name });
 });
 app.get("/about", (req, res) => {
-  res.render("about", {userName: req.session.name});
+  res.render("about", { userName: req.session.name });
 });
 
 app.post("/addUser", async (req, res) => {
@@ -119,12 +114,12 @@ app.post("/loginUser", async (req, res) => {
   }
 });
 
-app.get("/logout", (req,res)=>{
-  req.session.destroy((err)=>{
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
     console.log(err);
-  })
-  res.redirect("/login")
-})
+  });
+  res.redirect("/login");
+});
 
 app.listen(3000, () => {
   console.log("app running on port 3000");
